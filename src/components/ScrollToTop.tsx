4e040c4b0f139affba4b3b20e34c6ffa338760f4
eno.huang <eno.huang@zyxel.com.tw>
2024-07-08 22:48:38 +0800

    fix(style): Adjust style

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -14,7 +14,7 @@ function ScrollToTopButton() {
   return (
     <button
       aria-label="Scroll to top"
-      className={`z-100 fixed bottom-32 right-4 flex h-12 w-12 items-center justify-center rounded-full bg-[#8883] text-lg transition duration-300 hover:opacity-100 sm:right-32 print:hidden ${windowScrollY > 300 ? "opacity-75" : "pointer-events-none opacity-0"}`}
+      className={`z-100 fixed bottom-32 right-4 flex h-12 w-12 items-center justify-center rounded-full bg-[#8883] text-lg transition duration-300 hover:opacity-100 sm:right-32 print:hidden ${windowScrollY > 300 ? "opacity-50" : "pointer-events-none opacity-0"}`}
       onClick={toTop}
     >
       <Icon icon="flowbite:caret-up-outline" className="text-3xl" />
