95c7e76db1f0e1698bc7e4587cc24122980bf45a
eno.huang <eno.huang@zyxel.com.tw>
2024-07-30 15:27:53 +0800

    refactor(useTheme): use hook use control theme

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,32 +1,12 @@
+import { useTheme } from "@/hooks/useTheme";
 import { Icon } from "@iconify/react";
-import { useEffect, useState } from "react";
 
 function DarkModeToggle() {
-  const [isDark, setIsDark] = useState(false);
-
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    setIsDark(savedTheme === "dark");
-  }, []);
-
-  useEffect(() => {
-    if (isDark) {
-      document.documentElement.classList.add("dark");
-
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  }, [isDark]);
-
-  const toggleDark = () => {
-    setIsDark((prev) => !prev);
-  };
+  const [theme, toggleTheme] = useTheme();
 
   return (
-    <button onClick={toggleDark} className="nav-link text-xl dark:text-white">
-      {isDark ? (
+    <button onClick={toggleTheme} className="nav-link text-xl dark:text-white">
+      {theme == "dark" ? (
         <Icon icon="solar:moon-linear" />
       ) : (
         <Icon icon="solar:sun-linear" />
